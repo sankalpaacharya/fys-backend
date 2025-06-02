b106269ef8237210dd1b7cb3fc5e8fa0a45a0527
sankalpaacharya <sankalpaacharya01@gmail.com>
2025-06-02 23:56:23 +0530

    add frontend url in allow host

--- app/main.py
+++ app/main.py
@@ -1,7 +1,6 @@
 from fastapi import FastAPI
 from app.routes import route
 from fastapi.middleware.cors import CORSMiddleware
-from app.config.settings import settings
 
 app = FastAPI(title="FYS Backend")
 
@@ -9,7 +8,7 @@ app.include_router(route.router)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "http://127.0.0.1:3000"],
+    allow_origins=["http://localhost:3000", "http://127.0.0.1:3000","https://fixyourspend.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
