b19d184d9c52d77769e3c7b5a2bb8832b51f65a9
ronakrpanchal <rhtpanchal76@gmail.com>
2025-06-05 22:14:40 +0530

    updated env var

--- app/config/settings.py
+++ app/config/settings.py
@@ -5,10 +5,13 @@ class Setting(BaseSettings):
     OPENAI_API_KEY : str
     SUPABASE_URL : str
     SUPABASE_KEY : str
-
+    FAST_API_KEY : str
+    ENVIRONMENT : str = "development"
+    SITE_URL : str
+    NEXT_PUBLIC_SUPABASE_URL : str
+    NEXT_PUBLIC_SUPABASE_ANON_KEY : str
     
     class Config:
         env_file = ".env"
-        
         
 settings = Setting()
--- app/services/supabase_service.py
+++ app/services/supabase_service.py
@@ -1,16 +1,13 @@
-import os
 from app.config.settings import settings
 
-url: str = settings.SUPABASE_URL
-key: str = settings.SUPABASE_KEY
+url: str = settings.NEXT_PUBLIC_SUPABASE_URL
+key: str = settings.NEXT_PUBLIC_SUPABASE_ANON_KEY
 from supabase import AsyncClient,create_async_client
 
 async def create_supabase() -> AsyncClient:
     return await create_async_client(url,key)
 
-
 async def get_finance():
     supabase = await create_supabase()
     response = await supabase.table("finance").select("data").eq("user_id","9468f9b3-5d78-44b4-b714-e1aaff0195ef").execute()
     return response.data[0]
-
